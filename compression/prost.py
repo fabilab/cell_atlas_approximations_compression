852bcffeee7ec15a5905ea308180b3de6ad32cf5
Fabio Zanini <fabio.zanini@unsw.edu.au>
2024-04-16 16:19:13 +1000

    Bugfixes in PROST

--- compression/prost.py
+++ compression/prost.py
@@ -13,18 +13,6 @@ import torch
 from esm.data import Alphabet
 
 
-def create_parser():
-    parser = argparse.ArgumentParser(
-        description="Extract per-sequence representations and model outputs for sequences in multiple FASTA files",
-    )
-    parser.add_argument("--nogpu", action="store_true", help="Do not use GPU even if available")
-    parser.add_argument('--species', type=str, default='')
-    parser.add_argument('--overwrite', action='store_true')
-    parser.add_argument('--max-sequences', type=int, default=-1)
-    parser.add_argument('--no-save', action='store_true')
-    parser.add_argument('--use-approximations', action='store_true')
-    return parser
-
 
 def iterate_h5(h5file):
     with h5py.File(h5file) as fh:
@@ -39,7 +27,7 @@ def iterate_h5(h5file):
 def iterate_fasta(fastafile):
     import gzip
     from itertools import groupby
-    with gzip.open(fastafile) as fh:
+    with gzip.open(fastafile, 'rt') as fh:
         faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
         for header in faiter:
             header = next(header)[1:].strip()
@@ -58,6 +46,11 @@ def sanitise_name(name):
     return name
 
 
+def sanitise_sequence(seq):
+    """Optional stop codon."""
+    return seq.split('*')[0]
+
+
 def embed_protein_sequence(seq, model, batch_converter):
     '''Embed protein sequence into vector space'''
     def _embed(seq):
@@ -69,7 +62,7 @@ def embed_protein_sequence(seq, model, batch_converter):
 
         # Call ESM1b on the tokens
         results = model(toks)
-        
+
         # Fetch results
         for i in range(len(results)):
             results[i] = results[i].to(device="cpu")[0].detach().numpy()
@@ -101,8 +94,8 @@ def quantise_protein_vector_2D(emb, n=5, m=44):
         M = np.max(v)
         m = np.min(v)
         return (v - m) / float(M - m)
-    
-    
+
+
     def _iDCTquant(v, n):
         f = dct(v.T, type=2, norm='ortho')
         trans = idct(f[:, :n], type=2, norm='ortho')
@@ -124,7 +117,16 @@ def quantise_protein_vector_2D(emb, n=5, m=44):
 
 if __name__ == "__main__":
 
-    parser = create_parser()
+    parser = argparse.ArgumentParser(
+        description="Extract per-sequence representations and model outputs for sequences in multiple FASTA files",
+    )
+    parser.add_argument("--nogpu", action="store_true", help="Do not use GPU even if available")
+    parser.add_argument('--species', type=str, default='')
+    parser.add_argument('--overwrite', action='store_true')
+    parser.add_argument('--max-sequences', type=int, default=-1)
+    parser.add_argument('--no-save', action='store_true')
+    parser.add_argument('--use-approximations', action='store_true')
+
     args = parser.parse_args()
 
     if not args.use_approximations:
@@ -146,97 +148,111 @@ if __name__ == "__main__":
                 raise ValueError(f'Species not found: {organism}')
             input_files.append(fn)
             tmp.remove(fn)
- 
+
     # Output file
-    fn_out = pathlib.Path(f'embeddings_prost.h5')
+    fn_out = pathlib.Path('../data/esm/embeddings/prost_embeddings.h5')
 
-    print('Build ESM1b model for PROST')
-    model = torch.jit.load('traced_esm1b_25_13.pt').eval()
-    alphabet = Alphabet.from_architecture("ESM-1b")
-    batch_converter = alphabet.get_batch_converter()
-    #https://stackoverflow.com/a/63616077
-    #This prevents memory leak
-    for param in model.parameters():
-        param.grad = None
-        param.requires_grad = False
-    print('Transfer model to GPU')
-    if torch.cuda.is_available() and not args.nogpu:
-        model = model.cuda()
-        print("Transferred model to GPU")
-    else:
-        torch._C._jit_set_profiling_mode(False)
-        model = torch.jit.freeze(model)
-        model = torch.jit.optimize_for_inference(model)
-    
-    for input_file in input_files:
-        species = fasta_file.split('/')[-1].split('.')[0]
-        print(species)
+    try:
+        print('Build ESM1b model for PROST')
+        model = torch.jit.load('../data/esm/models/traced_esm1b_25_13.pt').eval()
+        alphabet = Alphabet.from_architecture("ESM-1b")
+        batch_converter = alphabet.get_batch_converter()
 
-        with h5py.File(fn_out, 'a') as h5:
-            if (not args.overwrite) and species in h5:
-                print('Already exists, skipping.')
+        #https://stackoverflow.com/a/63616077
+        #This prevents memory leak
+        for param in model.parameters():
+            param.grad = None
+            param.requires_grad = False
+
+        print('Transfer model to GPU')
+        if torch.cuda.is_available() and not args.nogpu:
+            model = model.cuda()
+            print("Transferred model to GPU")
+        else:
+            torch._C._jit_set_profiling_mode(False)
+            model = torch.jit.freeze(model)
+            model = torch.jit.optimize_for_inference(model)
+
+        for input_file in input_files:
+            species = input_file.split('/')[-1].split('.')[0]
+            print(species)
+
+            with h5py.File(fn_out, 'a') as h5:
+                if (not args.overwrite) and species in h5:
+                    print('Already exists, skipping.')
+                    continue
+
+            input_file = pathlib.Path(input_file)
+            if str(input_file).endswith('.fasta.gz'):
+                print('Read input sequences from FASTA.GZ file')
+                sequence_iter = iterate_fasta(input_file)
+            else:
+                print('Read input sequences from approximation (H5) file')
+                sequence_iter = iterate_h5(input_file)
+
+            sequence_labels = []
+            sequence_representations = []
+            times = []
+            lengths = []
+            errors = []
+            with torch.inference_mode():
+                for i, (name, seq) in enumerate(sequence_iter):
+                    print(f"Processing sequence {i + 1}", end='\r')
+                    name = sanitise_name(name)
+                    seq = sanitise_sequence(seq)
+                    # Too short sequences make no sense anyway
+                    if len(seq) < 10:
+                        continue
+
+                    try:
+                        t0 = time.time()
+                        # Embed in chunks if longer than 1022
+                        esm_output = embed_protein_sequence(seq, model, batch_converter)
+                        q25_544 = quantise_protein_vector_2D(esm_output[1], 5, 44)
+                        q13_385 = quantise_protein_vector_2D(esm_output[0], 3, 85)
+                        quantised = np.concatenate([q25_544,q13_385])
+                        t1 = time.time()
+                        if len(quantised) != 475:
+                            lq = len(quantised)
+                            raise ValueError(f"Quantisation for {name} gave length: {lq}")
+                    except:
+                        print()
+                        print(name, seq)
+                        errors.append((name, seq))
+                        raise
+                    else:
+                        # Accumulate outputs
+                        sequence_labels.append(name)
+                        sequence_representations.append(quantised)
+                        lengths.append(len(seq))
+                        times.append(t1 - t0)
+
+                    if (args.max_sequences != -1) and (i + 1 >= args.max_sequences):
+                        break
+                print(f'All done: {i+1} sequences')
+
+            if len(sequence_representations) == 0:
+                print('No sequences, skipping')
                 continue
 
-        print('Read FASTA')
-        input_file = pathlib.Path(input_file)
-        if input_file.endswith('fasta'):
-            sequence_iter = iterate_fasta(input_file)
-        else:
-            sequence_iter = iterate_h5(input_file)
+            sequence_representations = np.vstack(sequence_representations)
 
-        sequence_labels = []
-        sequence_representations = []
-        times = []
-        lengths = []
-        errors = []
-        with torch.inference_mode():
-            for i, (name, seq) in enumerate(fasta_iter):
-                print(f"Processing sequence {i + 1}", end='\r')
-                # Too short sequences make no sense anyway
-                if len(seq) < 10:
-                    continue
-                name = sanitise_name(name)
-                try:
-                    t0 = time.time()
-                    # Embed in chunks if longer than 1022
-                    esm_output = embed_protein_sequence(seq, model, batch_converter)
-                    q25_544 = quantise_protein_vector_2D(esm_output[1], 5, 44)
-                    q13_385 = quantise_protein_vector_2D(esm_output[0], 3, 85)
-                    quantised = np.concatenate([q25_544,q13_385])
-                    t1 = time.time()
-                    if len(quantised) != 475:
-                        raise ValueError(f"Quantisation for {name} gave length: len(quantised)")
-                except:
-                    print()
-                    print(name, seq)
-                    errors.append((name, seq))
-                    raise
-                else:
-                    # Accumulate outputs
-                    sequence_labels.append(name)
-                    sequence_representations.append(quantised)
-                    lengths.append(len(seq))
-                    times.append(t1 - t0)
+            if not args.no_save:
+                print('Store to file')
+                comp_kwargs = hdf5plugin.Zstd(clevel=22)
+                with h5py.File(fn_out, 'a') as h5:
+                    if species in h5:
+                        h5.pop(species)
+                    speciesg = h5.create_group(species)
+                    speciesg.create_dataset('features', data=np.array(sequence_labels).astype('S'))
+                    speciesg.create_dataset('embeddings', data=sequence_representations, dtype='u1')
 
-                if (args.max_sequences != -1) and (i + 1 >= args.max_sequences):
-                    break
-            print(f'All done: {i+1} sequences')
+            del times, lengths, sequence_representations, sequence_labels
+            gc.collect()
 
-        if len(sequence_representations) == 0:
-            print('No sequences, skipping')
-            continue
-
-        sequence_representations = np.vstack(sequence_representations)
-
-        if not args.no_save:
-            print('Store to file')
-            comp_kwargs = hdf5plugin.Zstd(clevel=22)
-            with h5py.File(fn_out, 'a') as h5:
-                if species in h5:
-                    h5.pop(species)
-                speciesg = h5.create_group(species)
-                speciesg.create_dataset('features', data=np.array(sequence_labels).astype('S'))
-                speciesg.create_dataset('embeddings', data=sequence_representations, dtype='u1')
-
-        del times, lengths, sequence_representations, sequence_labels
+    except:
+        print('Exception raised, removing model to clean GPU...')
+        del model, alphabet, batch_converter
         gc.collect()
+        print('Done, reraising exception...')
+        raise
--- compression/utils/paths.py
+++ compression/utils/paths.py
@@ -2,6 +2,7 @@ import pathlib
 
 
 root_repo_folder = pathlib.Path(__file__).parent.parent.parent
+
 # Try to put the output in the API repo if available
 output_folder = root_repo_folder / '..' / 'cell_atlas_approximations_API' / 'web' / 'static' / 'atlas_data'
 if not output_folder.is_dir():
