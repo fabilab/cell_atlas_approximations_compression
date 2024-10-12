32d17f2c676f057cfdc5b15a28384d200fcadb05
Fabio Zanini <fabio.zanini@fastmail.fm>
2024-10-13 10:11:40 +1100

    Ignore sequences from old-format h5 files, they do not have any

--- compression/utils/sequences.py
+++ compression/utils/sequences.py
@@ -8,7 +8,7 @@ import pandas as pd
 from .paths import root_repo_folder
 
 
-peptide_sequence_fdn = '../data/peptide_sequences/'
+peptide_sequence_fdn = "../data/peptide_sequences/"
 
 
 def collect_store_feature_sequences(
@@ -17,12 +17,13 @@ def collect_store_feature_sequences(
     measurement_type,
     species,
     fn_out,
-    compression=22, 
+    compression=22,
     min_fraction=0.3,
     fn_compressed_backup=None,
-    ):
+):
     if compression:
         import hdf5plugin
+
         # NOTE: decompressing zstd is equally fast no matter how much compression.
         # As for compression speed, levels 1-19 are normal, 20-22 "ultra".
         # A quick runtime test shows *faster* access for clevel=22 than clevel=3,
@@ -34,31 +35,31 @@ def collect_store_feature_sequences(
     else:
         comp_kwargs = {}
 
-    if 'feature_sequences' not in config_mt:
-        algo = 'bulk'
+    if "feature_sequences" not in config_mt:
+        algo = "bulk"
     else:
-        algo = config_mt['feature_sequences'].get('algorithm', 'bulk')
-    if algo == 'bulk':
+        algo = config_mt["feature_sequences"].get("algorithm", "bulk")
+    if algo == "bulk":
         fun = _collect_store_feature_sequences_bulk
     else:
         fun = _collect_store_feature_sequences_variable
 
-    fn_peptides = pathlib.Path(f'{peptide_sequence_fdn}/{species}.fasta.gz')
+    fn_peptides = pathlib.Path(f"{peptide_sequence_fdn}/{species}.fasta.gz")
     if fn_peptides.exists():
         path = fn_peptides
-        feature_type = 'protein'
+        feature_type = "protein"
     elif fn_compressed_backup is not None:
-        print('peptide file not found, using old approximation as a backup')
+        print("peptide file not found, using old approximation as a backup")
         path = fn_compressed_backup
-        feature_type = 'protein'
+        feature_type = "protein"
         fun = _collect_store_feature_sequences_bulk_backup
     else:
         raise IOError(
-            f'peptide file not found for species: {species}',
+            f"peptide file not found for species: {species}",
         )
-        #print('Peptide fasta.gz file not found, falling back onto config file...')
-        #path = root_repo_folder / 'data' / 'full_atlases' / measurement_type / species / config_mt['feature_sequences']['path']
-        #feature_type = config_mt['feature_sequences']['type']
+        # print('Peptide fasta.gz file not found, falling back onto config file...')
+        # path = root_repo_folder / 'data' / 'full_atlases' / measurement_type / species / config_mt['feature_sequences']['path']
+        # feature_type = config_mt['feature_sequences']['type']
 
     return fun(
         config_mt,
@@ -83,43 +84,53 @@ def _collect_store_feature_sequences_bulk_backup(
     fn_out,
     comp_kwargs,
     min_fraction,
-    ):
+):
     """Collect sequences of features and store to file, all at once (better comp)."""
     import hdf5plugin
 
     # 1. Collect sequences
-    seqs = {fea: '' for fea in features}
+    seqs = {fea: "" for fea in features}
     with h5py.File(path) as f:
-        group = f['measurements']['gene_expression']
-        for gene, seq in zip(group['var_names'].asstr(), group['feature_sequences']['sequences'].asstr()):
+        # NOTE: old approximations do not have sequences anyway
+        if "measurements" not in f:
+            print("No sequences found in old approximation")
+            return
+
+        group = f["measurements"]["gene_expression"]
+        for gene, seq in zip(
+            group["var_names"].asstr(), group["feature_sequences"]["sequences"].asstr()
+        ):
             # Remove stop codon from old versions
-            seq = seq.rstrip('*')
+            seq = seq.rstrip("*")
             if gene in features:
                 seqs[gene] = seq
 
     seqs = pd.Series(seqs).loc[features]
 
     # 2. Verify that sequences got written
-    frac_nonempty = (seqs != '').mean()
+    frac_nonempty = (seqs != "").mean()
     if frac_nonempty < min_fraction:
         pct_nonempty = int(100 * frac_nonempty)
         min_pct = int(100 * min_fraction)
-        import ipdb; ipdb.set_trace()
+        import ipdb
+
+        ipdb.set_trace()
         raise ValueError(
-            f'Only {pct_nonempty}% (< {min_pct}%) of the features had a sequence',
+            f"Only {pct_nonempty}% (< {min_pct}%) of the features had a sequence",
         )
     pct_nonempty = int(100 * frac_nonempty)
-    print(f'Percentage of features with known sequence: {pct_nonempty}%')
+    print(f"Percentage of features with known sequence: {pct_nonempty}%")
 
     # 3. Store sequences
-    with h5py.File(fn_out, 'a') as h5_data:
-        me = h5_data['measurements'][measurement_type]
-        group = me.create_group('feature_sequences')
+    with h5py.File(fn_out, "a") as h5_data:
+        me = h5_data["measurements"][measurement_type]
+        group = me.create_group("feature_sequences")
         group.attrs["type"] = feature_type
 
         # Bulk strings seem to be compresed better
         group.create_dataset(
-            "sequences", data=seqs.values.astype('S'),
+            "sequences",
+            data=seqs.values.astype("S"),
             **comp_kwargs,
         )
 
@@ -134,64 +145,66 @@ def _collect_store_feature_sequences_bulk(
     fn_out,
     comp_kwargs,
     min_fraction,
-    ):
+):
     """Collect sequences of features and store to file, all at once (better comp)."""
 
     # 1. Collect sequences
     gene_raws = set()
-    seqs = {fea: '' for fea in features}
-    with gzip.open(path, 'rt') as f:
+    seqs = {fea: "" for fea in features}
+    with gzip.open(path, "rt") as f:
         for gene, seq in _SimpleFastaParser(f):
             gene_raws.add(gene)
             # FIXME: this is from the peptide file now
-            gene = gene.strip(' ')
+            gene = gene.strip(" ")
             ## Sometimes they need a gene/id combo from biomart
             ## Do this only if no finer regex is set.
-            #if "replace" not in config_mt["feature_sequences"]:
+            # if "replace" not in config_mt["feature_sequences"]:
             #    if '|' in gene:
             #        gene = gene.split('|')[0]
             #    if ' ' in gene:
             #        gene = gene.split()[0]
-            #else:
+            # else:
             #    pattern = config_mt["feature_sequences"]["replace"]["in"]
             #    repl = config_mt["feature_sequences"]["replace"]["out"]
             #    gene = re.sub(pattern, repl, gene)
 
-            if gene == '':
+            if gene == "":
                 continue
             if gene in features:
                 seqs[gene] = seq
-            #else:
+            # else:
             #    gene2 = gene.split('|')[1]
             #    missing.append(gene2)
 
-    
-    #features2 = features.str.split('|', expand=True).get_level_values(1)
+    # features2 = features.str.split('|', expand=True).get_level_values(1)
     ####import ipdb; ipdb.set_trace()
 
     seqs = pd.Series(seqs).loc[features]
 
     # 2. Verify that sequences got written
-    frac_nonempty = (seqs != '').mean()
+    frac_nonempty = (seqs != "").mean()
     if frac_nonempty < min_fraction:
         pct_nonempty = int(100 * frac_nonempty)
         min_pct = int(100 * min_fraction)
-        import ipdb; ipdb.set_trace()
+        import ipdb
+
+        ipdb.set_trace()
         raise ValueError(
-            f'Only {pct_nonempty}% (< {min_pct}%) of the features had a sequence',
+            f"Only {pct_nonempty}% (< {min_pct}%) of the features had a sequence",
         )
     pct_nonempty = int(100 * frac_nonempty)
-    print(f'Percentage of features with known sequence: {pct_nonempty}%')
+    print(f"Percentage of features with known sequence: {pct_nonempty}%")
 
     # 3. Store sequences
-    with h5py.File(fn_out, 'a') as h5_data:
-        me = h5_data['measurements'][measurement_type]
-        group = me.create_group('feature_sequences')
+    with h5py.File(fn_out, "a") as h5_data:
+        me = h5_data["measurements"][measurement_type]
+        group = me.create_group("feature_sequences")
         group.attrs["type"] = feature_type
 
         # Bulk strings seem to be compresed better
         group.create_dataset(
-            "sequences", data=seqs.values.astype('S'),
+            "sequences",
+            data=seqs.values.astype("S"),
             **comp_kwargs,
         )
 
@@ -206,19 +219,19 @@ def _collect_store_feature_sequences_variable(
     fn_out,
     comp_kwargs,
     min_fraction,
-    ):
+):
     """Collect sequences of features and store to file (less RAM)."""
 
     featuress = pd.Series(np.arange(len(features)), index=features)
 
-    with h5py.File(fn_out, 'a') as h5_data, gzip.open(path, 'rt') as f:
-        me = h5_data['measurements'][measurement_type]
-        group = me.create_group('feature_sequences')
+    with h5py.File(fn_out, "a") as h5_data, gzip.open(path, "rt") as f:
+        me = h5_data["measurements"][measurement_type]
+        group = me.create_group("feature_sequences")
         group.attrs["type"] = feature_type
 
         # Variable length strings... seems like it's not compressed very well or at all
         seqs = group.create_dataset(
-            'sequences',
+            "sequences",
             shape=len(features),
             dtype=h5py.string_dtype(),
             **comp_kwargs,
@@ -229,20 +242,20 @@ def _collect_store_feature_sequences_variable(
         for gene, seq in _SimpleFastaParser(f):
             # Sometimes they need a gene/id combo from biomart
             # Do this only if no finer regex is set.
-            #if "replace" not in config_mt["feature_sequences"]:
+            # if "replace" not in config_mt["feature_sequences"]:
             #    if '|' in gene:
             #        gene = gene.split('|')[0]
             #    if ' ' in gene:
             #        gene = gene.split()[0]
-            #else:
+            # else:
             #    pattern = config_mt["feature_sequences"]["replace"]["in"]
             #    repl = config_mt["feature_sequences"]["replace"]["out"]
             #    gene = re.sub(pattern, repl, gene)
 
             # NOTE: uncomment to debug feature sequences
-            #import ipdb; ipdb.set_trace()
+            # import ipdb; ipdb.set_trace()
 
-            if gene == '':
+            if gene == "":
                 n_empty += 1
                 continue
 
@@ -255,7 +268,7 @@ def _collect_store_feature_sequences_variable(
             pct_nonempty = int(100 * frac_nonempty)
             min_pct = int(100 * min_fraction)
             raise ValueError(
-                f'Only {pct_nonempty}% (< {min_pct}%) of the features had a sequence',
+                f"Only {pct_nonempty}% (< {min_pct}%) of the features had a sequence",
             )
 
 
@@ -305,4 +318,3 @@ def _SimpleFastaParser(handle):
         lines.append(line.rstrip())
 
     yield title, "".join(lines).replace(" ", "").replace("\r", "")
-
